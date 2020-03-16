19dd06489a21ff7a6a33fcb33ff3a93527989ffb
pro-use <robertprouse@gmail.com>
2020-03-16 23:40:06 +0000

    fix map size

--- site/templates/map.php
+++ site/templates/map.php
@@ -4,18 +4,11 @@ $MAPS_API_KEY = apache_getenv("MAPS_API_KEY"); ?>
 <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-    <title>Using MySQL and PHP with Google Maps</title>
     <style>
       /* Always set the map height explicitly to define the size of the div
        * element that contains the map. */
       #map {
         height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
       }
     </style>
   </head>
