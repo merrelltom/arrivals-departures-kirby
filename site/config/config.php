d5d0a4695dc982ead071002102f5225ac38f4891
pro-use <robertprouse@gmail.com>
2020-03-16 23:23:47 +0000

    add get_map as snippet

--- site/config/config.php
+++ site/config/config.php
@@ -1,5 +1,22 @@
 <?php
 return [
-    'debug' => true
+    'debug' => true,
+    'routes' => [
+  [
+    'pattern' => 'sitemap.xml',
+    'action'  => function() {
+        $content = snippet('get_map', true);
+
+        // return response with correct header type
+        return new Kirby\Cms\Response($content, 'application/xml');
+    }
+  ],
+  [
+    'pattern' => 'sitemap',
+    'action'  => function() {
+      return go('sitemap.xml', 301);
+    }
+  ]
+]
 ];
 ?>
