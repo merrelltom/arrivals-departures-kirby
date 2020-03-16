147029090f5d0aa940c564d944f0571289cd0dc4
pro-use <robertprouse@gmail.com>
2020-03-16 22:48:37 +0000

    add oauth to get_map

--- site/templates/get_map.php
+++ site/templates/get_map.php
@@ -1,12 +1,12 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+//
+//snippet('oauth');
+//$token = getToken();
 
-snippet('oauth');
-$token = getToken();
-
-echo($token);
+echo("test");
 
 //$arr_curl = curl_init();
 //curl_setopt_array($arr_curl, [
