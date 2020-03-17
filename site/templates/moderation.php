4e295deee1b581d1c1316c8a7650aad67c23cc93
pro-use <robertprouse@gmail.com>
2020-03-17 15:35:57 +0000

    Add moderation page

--- site/templates/moderation.php
+++ site/templates/moderation.php
@@ -1,3 +1,9 @@
-<?php
+<?php snippet('header') ?>
 
+    <main class="main"> 
 
+    <?php snippet('arrivals-moderation') ?> 
+    <?php snippet('departures-moderation') ?> 
+
+    </main>
+<?php snippet('footer') ?>
