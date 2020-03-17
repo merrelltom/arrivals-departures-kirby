dd6e203ba29db9750e3936258ec1de3890a3d0aa
pro-use <robertprouse@gmail.com>
2020-03-17 16:00:40 +0000

    check for post vals

--- site/templates/moderation.php
+++ site/templates/moderation.php
@@ -1,11 +1,11 @@
 <?php snippet('header'); 
     snippet('oauth');
     $token = getToken();
+    $update_type = filter_input(INPUT_POST, "type");
+    $update_type;
 ?>
     
 <main class="main"> 
-
     <?php snippet('arrivals-moderation', ['token'=>$token]) ?> 
     <?php snippet('departures-moderation', ['token'=>$token]) ?> 
-
 </main>
