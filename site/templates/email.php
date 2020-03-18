472784bce6b8640b95baa99caa9b3a553ed1ea3a
pro-use <robertprouse@gmail.com>
2020-03-18 15:40:13 +0000

    update email template css

--- site/templates/email.php
+++ site/templates/email.php
@@ -107,7 +107,7 @@
                       </td>
                     </tr>
                     <tr>
-                      <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">
+                      <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; padding-bottom: 20px">
                         <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; box-sizing: border-box;">
                           <tbody>
                             <tr>
