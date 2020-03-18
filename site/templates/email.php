18be3a02e432d668b9dcef06c8a0aa3b4b211393
pro-use <robertprouse@gmail.com>
2020-03-18 16:05:39 +0000

    update email template css

--- site/templates/email.php
+++ site/templates/email.php
@@ -100,6 +100,12 @@
               <tr>
                 <td class="wrapper" style="font-family: sans-serif; font-size: 14px; vertical-align: top; box-sizing: border-box;">
                   <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;">
+                    <!--icon -->
+                    <tr>
+                        <td style="font-family: sans-serif; font-size: 24px; vertical-align: top;">
+                            <p>ARRIVALS +<br>DEPARTURES </p>
+                        </td>
+                    </tr>
                     <!--yellow top table w/ -->
                     <tr>
                       <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">
