b788f2612ac1ebf1dc3011922e01b3014429b4f5
pro-use <robertprouse@gmail.com>
2020-03-19 13:22:59 +0000

    update email template content

--- site/templates/email.php
+++ site/templates/email.php
@@ -128,6 +128,8 @@
                                       <td style="font-family: sans-serif; font-size: 0.875rem; vertical-align: top; background-color: #F1F900; border-radius: 18px; padding-left: 30px"> 
                                           <p>An interactive public installation about birth, death and the journey in between by YARA + DAVINA</p>
                                       </td>
+                                    </tr>
+                                    <tr>
                                       <td style="font-family: sans-serif; font-size: 0.875rem; vertical-align: top; background-color: #F1F900; border-radius: 18px; padding-left: 30px"> 
                                           <a href="http://www.arrivalsanddepartures.net" target="_blank" style="display: inline-block; color: #000; background-color: #F1F900; border: solid 1px #000; border-radius: 18px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 0.875rem; margin: 0; padding: 0.4rem 2rem 0.3rem; text-transform: capitalize; border-color: #000;">Go to the online arrivals and departures boards</a>
                                       </td>
