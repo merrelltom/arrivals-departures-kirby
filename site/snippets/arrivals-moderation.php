8d34e0ab6c8a036a2407939b793fdba6ced046d7
Tom Merrell <tom@merrelltom.com>
2020-03-25 16:54:46 +0000

    add a or d to differntiate IDs on arrivals and departures moderation

--- site/snippets/arrivals-moderation.php
+++ site/snippets/arrivals-moderation.php
@@ -26,7 +26,7 @@ if (count($results) == 0) {
     echo("<div class='row'><h2 class='col-xs-12 section-title moderation-title'><hr>Arrivals:</h2></div>");
 }
 foreach ($results as $result) { ?>
-    <form method="post" action="./moderation" class="row moderation-row" id="board-line-<?php echo($board_line);?>">
+    <form method="post" action="./moderation" class="row moderation-row" id="a-board-line-<?php echo($board_line);?>">
         <div class="col-xs-12"><hr></div>
         <div class="col-xs-12 col-md-2 large-text"><?php echo($result["date"]);?></div>
         <div class="col-xs-12 col-md-4 large-text"><?php echo($result["name"]);?></div>        
@@ -34,14 +34,14 @@ foreach ($results as $result) { ?>
         <input type="hidden" value="<?php echo($result["ID"]);?>" name="ID" id="ID">
         <div class="radio-wrapper col-xs-12 col-md-2">
             <div>
-                <input type="radio" id="delete-<?php echo($board_line);?>" name="update" value="delete">
-                <label for="delete-<?php echo($board_line);?>"><span class="label">Delete</span></label>
+                <input type="radio" id="delete-a-<?php echo($board_line);?>" name="update" value="delete">
+                <label for="delete-a-<?php echo($board_line);?>"><span class="label">Delete</span></label>
             </div>
         </div>
         <div class="radio-wrapper col-xs-12 col-md-2">
             <div>
-                <input type="radio" id="accept-<?php echo($board_line);?>" name="update" value="accept">
-                <label for="accept-<?php echo($board_line);?>"><span class="label">Accept</span></label>
+                <input type="radio" id="accept-a-<?php echo($board_line);?>" name="update" value="accept">
+                <label for="accept-a-<?php echo($board_line);?>"><span class="label">Accept</span></label>
             </div>
         </div>
         <div class="col-xs-12 col-md-2">
