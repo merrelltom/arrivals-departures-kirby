d9b8b0f6bb5e126db5465fd7ce59af1597c33fe9
pro-use <robertprouse@gmail.com>
2020-03-26 12:04:29 +0000

    add get_email route + logic

--- site/snippets/get_email.php
+++ site/snippets/get_email.php
@@ -1,19 +1,19 @@
 <?php
 
 if ($type == 'confirm' ) {
-    $email_text = $email->confirmtext()->kt();
+    $email_text = array('email_text'=> $email->confirmtext()->kt());
     echo json_encode($email_text);
 } elseif ($type == 'moderate') {
-    $email_text = $email->moderationtext()->kt();
+    $email_text = array('email_text'=> $email->moderationtext()->kt());
     echo json_encode($email_text);
 } elseif ($type == 'display') {
-    $email_text = $email->displaytext()->kt();
+    $email_text = array('email_text'=> $email->displaytext()->kt());
     echo json_encode($email_text);
 } elseif ($type == 'reject') {
-    $email_text = $email->rejecttext()->kt();
+    $email_text = array('email_text'=> $email->rejecttext()->kt());
     echo json_encode($email_text);
 } else {
-    $email_text = NULL;
+    array('email_text'=> $email_text = NULL);
     echo json_encode($email_text);
 }
 
