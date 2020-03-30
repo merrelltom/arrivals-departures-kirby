28fdf134479a61aca95af37e0ddcad817a67dffa
pro-use <robertprouse@gmail.com>
2020-03-30 13:48:58 +0100

    return email subject

--- site/config/config.php
+++ site/config/config.php
@@ -16,18 +16,25 @@ return [
     'action'  => function($any) {
       $email = site()->children()->findByURI('email');
       if ($any == 'confirm' ) {
-
-        $email_text = $email->confirmtext()->kt();
+        $email_text = array(
+            "subject"=>$email->confirmsubject()->kt(),
+            "text"=>$email->confirmtext()->kt());
       } elseif ($any == 'moderate') {
-        $email_text = $email->moderationtext()->kt();
+        $email_text = array(
+            "subject"=>$email->moderationsubject()->kt(),
+            "text"=>$email->moderationtext()->kt());
       } elseif ($any == 'display') {
-        $email_text = $email->displaytext()->kt();
+        $email_text = array(
+            "subject"=>$email->displaysubject()->kt(),
+            "text"=>$email->displaytext()->kt());
       } elseif ($any == 'reject') {
-        $email_text = $email->rejecttext()->kt();
+        $email_text = array(
+            "subject"=>$email->rejectsubject()->kt(),
+            "text"=>$email->rejecttext()->kt());
       } else {
         $email_text = NULL;   
       }
-      $email_text = json_encode($email_text);
+      #$email_text = json_encode($email_text);
       // return response with correct header type
       return new Kirby\Cms\Response($email_text, 'application/json');
     }
