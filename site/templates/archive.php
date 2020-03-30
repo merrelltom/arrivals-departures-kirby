8a24754f76f68ea476d61c518b6748473b59afc0
Tom Merrell <tom@merrelltom.com>
2020-03-30 14:02:37 +0100

    archive templates part 6

--- site/templates/archive.php
+++ site/templates/archive.php
@@ -1,7 +1,6 @@
 <?php snippet('header') ?>
 
     <main class="main">
-        
         <section class="page-section">
             <div class="wrapper">
                 <div class="row">
@@ -12,19 +11,18 @@
                 </div>
             </div>
         </section>
-        
         <section class="page-section">
             <div class="wrapper">
                 <div class="row">
                     <div class="col-xs-12 col-lg-6">
                         <h2 class="section-title">Arrivals</h2>
-                        <ol class="names-list large-text">
+                        <ol class="names-list">
                             <?php snippet('arrivals-archive');?>
                         </ol>
                     </div>
                     <div class="col-xs-12 col-lg-6">
                         <h2 class="section-title">Departures</h2>
-                        <ol class="names-list large-text">
+                        <ol class="names-list">
                            <?php snippet('departures-archive');?>
                         </ol>
                     </div>
