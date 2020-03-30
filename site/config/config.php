a1217d2f1e5f58ebb80a7d1ada74683ff1fa7f72
pro-use <robertprouse@gmail.com>
2020-03-30 13:49:36 +0100

    return email subject

--- site/config/config.php
+++ site/config/config.php
@@ -34,7 +34,7 @@ return [
       } else {
         $email_text = NULL;   
       }
-      #$email_text = json_encode($email_text);
+      $email_text = json_encode($email_text);
       // return response with correct header type
       return new Kirby\Cms\Response($email_text, 'application/json');
     }
