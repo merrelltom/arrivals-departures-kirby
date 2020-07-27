1aee2ae05f0ca018744a9cc79e87afa3ed2f9857
Tom Merrell <tom@MacBook-Pro.local>
2020-07-27 15:52:09 +0100

    add flow flow pt8

--- site/templates/socialfeed.php
+++ site/templates/socialfeed.php
@@ -1,5 +1,5 @@
 <?php snippet('header');?>
-<?php require_once("http://134.209.184.8/admin/flow-flow/ff-injector.php"); $injector = new FFInjector(); ?>
+<?php require_once("http://134.209.184.8/var/www/arrivals-departures/admin/flow-flow/ff-injector.php"); $injector = new FFInjector(); ?>
 <?php echo $injector->head(true, false); ?>
 <?php $injector->stream(1); ?>
 <?php snippet('footer');?>
