b7e306111e6a35aec4a5f6a76b6f3ee5abfa68ca
Tom Merrell <tom@MacBook-Pro.local>
2020-07-27 17:38:45 +0100

    remove flow flow plugin. Add Juicer plugin

--- site/templates/socialfeed.php
+++ site/templates/socialfeed.php
@@ -1,5 +1,34 @@
 <?php snippet('header');?>
-<?php require_once("http://134.209.184.8/admin/flow-flow/ff-injector.php"); $injector = new FFInjector(); ?>
-<?php echo $injector->head(true, false); ?>
-<?php $injector->stream(1); ?>
+
+    <main class="main">
+        
+        <section class="page-section">
+            <div class="wrapper">
+                <div class="row">
+                    <h2 class="section-title col-xs-12 col-md-4 col-lg-5"><?= $page->pageTitle();?></h2>
+                    <div class="introduction large-text col-xs-12 col-md-8 col-lg-7">
+                        <?= $page->pageIntroduction()->kt(); ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <?php
+        foreach($page->blocks()->toBuilderBlocks() as $block):
+          snippet('sections/' . $block->_key(), array('data' => $block));
+        endforeach;
+        ?>
+        
+
+        <section class="page-section">
+            <div class="wrapper">
+
+                <script src="https://assets.juicer.io/embed.js" type="text/javascript"></script>
+                <link href="https://assets.juicer.io/embed.css" media="all" rel="stylesheet" type="text/css" />
+                <ul class="juicer-feed" data-feed-id="london-ac92fb07-7bc2-45cc-bc77-c61122dcd0ae"></ul>
+                
+            </div>
+        </section>
+</main>
+
 <?php snippet('footer');?>
