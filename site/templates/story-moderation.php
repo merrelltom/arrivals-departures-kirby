c50f2cabab7914e9ba90b1beb071fdbd4962c01d
pro-use <robertprouse@gmail.com>
2020-07-28 15:50:55 +0100

    Add story moderation

---
+++ site/templates/story-moderation.php
@@ -0,0 +1,47 @@
+<?php snippet('header'); 
+    snippet('oauth');
+    $token = getToken();
+    $update_type = filter_input(INPUT_POST, "type");
+    $action = filter_input(INPUT_POST, "update");
+    $id = filter_input(INPUT_POST, "ID");?>
+<main class="main"> 
+    <?php if (!$kirby->user()): ?>
+    <section class="page-section bg-yellow">
+        <div class="wrapper">
+            <div class="row">
+                <div class="col-xs-12">
+                   <h2 class="section-title">User Message: You must be logged in to moderate name entries. </h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php else : ?>
+    <section class="page-section mod-result">
+        <div class="wrapper">
+            <div class="row">
+                <div class="col-xs-12">
+                    <?php 
+                    if ($update_type == 'arrival'){
+                        snippet('arrivals-update-story', ['token'=>$token, 'action'=>$action, 'ID'=>$id]);
+                    } elseif ($update_type == 'departure'){
+                        snippet('departures-update-story', ['token'=>$token, 'action'=>$action, 'ID'=>$id]);
+                    } ?>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section class="page-section moderation-forms arrival-moderation">
+        <div class="wrapper">
+            <?php snippet('arrivals-moderate-story', ['token'=>$token]) ?>
+        </div>
+    </section>
+
+    <section class="page-section moderation-forms arrival-moderation">
+        <div class="wrapper">
+            <?php snippet('departures-moderate-story', ['token'=>$token]) ?> 
+        </div>
+    </section>
+    
+    <?php endif; ?>
+</main>
