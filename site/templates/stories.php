3a7992bd681b246239f8da22a62d741513a49bd8
pro-use <robertprouse@gmail.com>
2020-07-28 13:12:51 +0100

    add stories archive snippet

--- site/templates/stories.php
+++ site/templates/stories.php
@@ -17,17 +17,9 @@
         <section class="page-section">
             <div class="wrapper">
                 <div class="grid">
-                   <div id="2" class="col-xs-12 col-md-6 col-xl-4 resource">
-                        <div class="bg-yellow story">
-                            <div class="story-cat">Arrival</div>
-                            <h2 class="story-name large-text">Rob Prouse</h2>
-                            <div class="story-date">13.05.20</div>
-                            <div class="story-text"> 
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id sagittis arcu, ut tristique felis. Praesent tempus nec erat lacinia dapibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis in dolor eu enim molestie aliquam. Nulla non odio orci. Vestibulum volutpat id arcu in ultrices. Vestibulum faucibus lorem ut sapien sollicitudin elementum. Maecenas aliquam justo at fermentum dignissim. Donec ut lectus leo. Nulla augue ex, tempus eu massa eget, euismod tincidunt mi. Vestibulum pellentesque urna nec diam auctor, eu vehicula odio posuere. Praesent hendrerit lorem id urna interdum, vitae hendrerit ante euismod. Integer porta tortor ex, molestie euismod tellus blandit vel.
-                            </div>
-                        </div>
-                    </div>
-                    
+                  <?php 
+                    snippet('ad-stories');
+                  ?>  
                 </div>
             </div>
         </section>
