3dd3b135f68eaf0540f1c33cda69cfa4d1ad7b2f
Tom Merrell <tom@MacBook-Pro-2.local>
2021-03-24 12:13:46 +0000

    add settings and test page for automated open / close of submissions

---
+++ site/templates/opening-test-page.php
@@ -0,0 +1,40 @@
+<?php 
+    $open = true;
+    $currentTime = time();
+    $openTime = $site->openTime();
+    $closeTime = $site->closeTime();
+    if($site->submissionsOpen() == false){
+        $open = false;
+    }
+    if($site->automateSubmissions() == true){
+        if($time > $openTime && $time < $closeTime){
+            $open = true;
+        }else{
+            $open = false;
+        }
+    }
+
+    if($open == false):
+        echo 'closed';
+    endif;
+    if($open == true):
+        echo 'open';
+    endif;   
+    echo 'hr';
+    echo 'Current Time: ';    
+    echo  $currentTime;
+    echo 'hr';
+    echo 'Open Time: ';    
+    echo  $openTime;   
+    echo 'hr';
+    echo 'Close Time: ';
+    echo  $closeTime; 
+    echo 'hr';
+    echo 'Submissions open setting: ';
+    echo  $site->submissionsOpen(); 
+    echo 'Automate setting: ';
+    echo  $site->automateSubmissions();     
+    echo 'hr';
+    echo 'Open result: ';    
+    echo $open;           
+?>
