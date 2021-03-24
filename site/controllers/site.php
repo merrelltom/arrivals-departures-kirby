0d1c2de84d4a3040694775e9635153de843fc6be
Tom Merrell <tom@MacBook-Pro-2.local>
2021-03-24 13:58:29 +0000

    fix homepage bug

--- site/controllers/site.php
+++ site/controllers/site.php
@@ -1,15 +1,15 @@
 <?php 
-    $SubmissionsOpen = true;
-    $currentTime = date("H:i");
-    $openTime = $site->openTime()->toDate('H:i');
-    $closeTime = $site->closeTime()->toDate('H:i');
-    $open = $site->submissionsOpen()->toBool();
+    // $SubmissionsOpen = true;
+    // $currentTime = date("H:i");
+    // $openTime = $site->openTime()->toDate('H:i');
+    // $closeTime = $site->closeTime()->toDate('H:i');
+    // $open = $site->submissionsOpen()->toBool();
 
-    if($site->automateSubmissions()->toBool() == true){
-        if (time() >= strtotime($openTime) && time() <= strtotime($closeTime)){
-            $SubmissionsOpen = true;
-        }else{
-            $SubmissionsOpen = false;
-        }
-    }      
+    // if($site->automateSubmissions()->toBool() == true){
+    //     if (time() >= strtotime($openTime) && time() <= strtotime($closeTime)){
+    //         $SubmissionsOpen = true;
+    //     }else{
+    //         $SubmissionsOpen = false;
+    //     }
+    // }      
 ?>
